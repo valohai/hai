18a74308948c9d18838d0cf4a33de18eee9a8c8f
Aarni Koskela <akx@iki.fi>
2018-02-07 12:59:42 +0200

    parallel: embetter semantics for wait(), .return_values; add .exceptions

--- hai_tests/test_parallel.py
+++ hai_tests/test_parallel.py
@@ -29,3 +29,14 @@ def test_parallel_retval():
     parallel.add_task(return_true)
     parallel.wait()
     assert parallel.return_values == {'blerg': True, 'return_true': True}
+
+
+def test_parallel_wait_without_fail_fast():
+    parallel = ParallelRun()
+    parallel.add_task(return_true, name='true')
+    parallel.add_task(agh, name='agh')
+    parallel.wait(fail_fast=False)
+    assert parallel.exceptions['agh'].args[0] == 'agh!'
+    assert parallel.return_values['true'] is True
+    with pytest.raises(ParallelException):
+        parallel.maybe_raise()
