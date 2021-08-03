452616956f92f16e361753a253d047f1e880073f
Aarni Koskela <akx@iki.fi>
2021-08-03 17:17:56 +0300

    Fix up version specifiers

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ if __name__ == '__main__':
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
