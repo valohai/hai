1851ed7a75dadd9bdbfc524dcf12e2fcb7f536a7
Black <services@valohai.com>
2023-07-21 11:41:58 +0300

    Format with Black, with trailing commas

--- hai_tests/test_multipart_upload.py
+++ hai_tests/test_multipart_upload.py
@@ -18,28 +18,30 @@ class ChunkCallbackMultipartUploader(MultipartUploader):
 
 
 @mock_s3
-@pytest.mark.parametrize('file_type', ('real', 'imaginary'))
+@pytest.mark.parametrize("file_type", ("real", "imaginary"))
 @pytest.mark.parametrize(
-    'mpu_class', (MultipartUploader, ChunkCallbackMultipartUploader), ids=('no-func', 'chunk-func')
+    "mpu_class",
+    (MultipartUploader, ChunkCallbackMultipartUploader),
+    ids=("no-func", "chunk-func"),
 )
 def test_multipart_upload(tmpdir, file_type, mpu_class):
-    if file_type == 'real':
-        temp_path = tmpdir.join('temp.dat')
-        with temp_path.open('wb') as outf:
+    if file_type == "real":
+        temp_path = tmpdir.join("temp.dat")
+        with temp_path.open("wb") as outf:
             for chunk in range(17):
                 outf.write(bytes((chunk,)) * 1024 * 1024)
             expected_size = outf.tell()
-        file = temp_path.open('rb')
-    elif file_type == 'imaginary':
+        file = temp_path.open("rb")
+    elif file_type == "imaginary":
         expected_size = S3_MINIMUM_MULTIPART_FILE_SIZE * 2
-        file = BytesIO(b'\xC0' * expected_size)
+        file = BytesIO(b"\xC0" * expected_size)
         file.seek(0)
     else:  # pragma: no cover
-        raise NotImplementedError('...')
+        raise NotImplementedError("...")
 
-    s3 = boto3.client('s3', region_name='us-east-1')
-    bucket_name = 'mybucket'
-    key_name = 'hello/world'
+    s3 = boto3.client("s3", region_name="us-east-1")
+    bucket_name = "mybucket"
+    key_name = "hello/world"
     s3.create_bucket(Bucket=bucket_name)
     mpu = mpu_class(s3)
     events = []
@@ -47,7 +49,7 @@ def test_multipart_upload(tmpdir, file_type, mpu_class):
     def event_handler(**args):
         events.append(args)
 
-    mpu.on('*', event_handler)
+    mpu.on("*", event_handler)
 
     if mpu_class is ChunkCallbackMultipartUploader:
         mpu.chunk_sizes = []
@@ -56,8 +58,8 @@ def test_multipart_upload(tmpdir, file_type, mpu_class):
         mpu.upload_file(bucket_name, key_name, file)
 
     obj = s3.get_object(Bucket=bucket_name, Key=key_name)
-    assert obj['ContentLength'] == expected_size
-    assert any(e['event'] == 'progress' for e in events)
+    assert obj["ContentLength"] == expected_size
+    assert any(e["event"] == "progress" for e in events)
 
     if mpu_class is ChunkCallbackMultipartUploader:
         assert sum(mpu.chunk_sizes) == expected_size
@@ -65,31 +67,31 @@ def test_multipart_upload(tmpdir, file_type, mpu_class):
 
 @mock_s3
 def test_invalid_chunk_size():
-    s3 = boto3.client('s3', region_name='us-east-1')
+    s3 = boto3.client("s3", region_name="us-east-1")
     mpu = MultipartUploader(s3)
     with pytest.raises(ValueError):
-        mpu.upload_file('foo', 'foo', BytesIO(), chunk_size=300)
+        mpu.upload_file("foo", "foo", BytesIO(), chunk_size=300)
 
 
 @mock_s3
 def test_invalid_file_size(tmpdir):
-    s3 = boto3.client('s3', region_name='us-east-1')
-    pth = tmpdir.join('temp.dat')
-    pth.write('foofoo')
+    s3 = boto3.client("s3", region_name="us-east-1")
+    pth = tmpdir.join("temp.dat")
+    pth.write("foofoo")
     mpu = MultipartUploader(s3)
     with pytest.raises(ValueError):
-        mpu.upload_file('foo', 'foo', pth.open())
+        mpu.upload_file("foo", "foo", pth.open())
 
 
 @mock_s3
 def test_error_handling():
-    s3 = boto3.client('s3', region_name='us-east-1')
-    s3.create_bucket(Bucket='foo')
+    s3 = boto3.client("s3", region_name="us-east-1")
+    s3.create_bucket(Bucket="foo")
 
     def upload_fn(**args):
-        raise OSError('the internet is dead')
+        raise OSError("the internet is dead")
 
     s3.upload_part = upload_fn
     mpu = MultipartUploader(s3)
     with pytest.raises(IOError):
-        mpu.upload_parts('foo', 'foo', [b'\x00' * S3_MINIMUM_MULTIPART_FILE_SIZE])
+        mpu.upload_parts("foo", "foo", [b"\x00" * S3_MINIMUM_MULTIPART_FILE_SIZE])
--- hai_tests/test_parallel.py
+++ hai_tests/test_parallel.py
@@ -8,7 +8,7 @@ from hai.parallel import ParallelException, ParallelRun, TaskFailed, TasksFailed
 
 def agh():
     time.sleep(0.1)
-    raise RuntimeError('agh!')
+    raise RuntimeError("agh!")
 
 
 def return_true():
@@ -21,43 +21,43 @@ def test_parallel_crash():
         failing_task = parallel.add_task(agh)
         with pytest.raises(TaskFailed) as ei:
             parallel.wait(fail_fast=True)
-        assert str(ei.value.__cause__) == str(ei.value.exception) == 'agh!'
+        assert str(ei.value.__cause__) == str(ei.value.exception) == "agh!"
         assert ei.value.task == failing_task
 
 
 def test_parallel_retval():
     with ParallelRun() as parallel:
-        parallel.add_task(return_true, name='blerg')
+        parallel.add_task(return_true, name="blerg")
         parallel.add_task(return_true)
         parallel.wait()
-        assert parallel.return_values == {'blerg': True, 'return_true': True}
+        assert parallel.return_values == {"blerg": True, "return_true": True}
 
 
 def test_parallel_wait_without_fail_fast():
     with ParallelRun() as parallel:
-        parallel.add_task(return_true, name='true')
-        parallel.add_task(agh, name='agh')
+        parallel.add_task(return_true, name="true")
+        parallel.add_task(agh, name="agh")
         parallel.wait(fail_fast=False)
-        assert parallel.exceptions['agh'].args[0] == 'agh!'
-        assert parallel.return_values['true'] is True
+        assert parallel.exceptions["agh"].args[0] == "agh!"
+        assert parallel.return_values["true"] is True
         with pytest.raises(TasksFailed) as ei:
             parallel.maybe_raise()
         assert len(ei.value.exception_map) == 1
-        assert isinstance(ei.value.exception_map['agh'], RuntimeError)
-        assert ei.value.failed_task_names == {'agh'}
+        assert isinstance(ei.value.exception_map["agh"], RuntimeError)
+        assert ei.value.failed_task_names == {"agh"}
 
 
-@pytest.mark.parametrize('is_empty_run', (False, True))
+@pytest.mark.parametrize("is_empty_run", (False, True))
 def test_parallel_callback_is_called_at_least_once_on_wait(is_empty_run):
     with ParallelRun() as parallel:
         stub = MagicMock()
         if not is_empty_run:
-            parallel.add_task(return_true, name='true')
+            parallel.add_task(return_true, name="true")
         parallel.wait(callback=stub)
         stub.assert_called_with(parallel)
 
 
-@pytest.mark.parametrize('fail', (False, True))
+@pytest.mark.parametrize("fail", (False, True))
 def test_parallel_limit(fail):
     """
     Test that parallelism limits work.
@@ -70,7 +70,7 @@ def test_parallel_limit(fail):
         nonlocal count
         assert count <= 3
         count += 1
-        time.sleep(.1)
+        time.sleep(0.1)
         count -= 1
 
     with ParallelRun(parallelism=(5 if fail else 3)) as parallel:
@@ -89,7 +89,7 @@ def test_parallel_long_interval_interruptible():
     Test that even with long poll intervals, completion events interrupt the sleep
     """
     with ParallelRun() as parallel:
-        parallel.add_task(time.sleep, args=(.5,))  # will only wait for half a second
+        parallel.add_task(time.sleep, args=(0.5,))  # will only wait for half a second
         t0 = time.time()
         parallel.wait(interval=10)  # would wait for 10
         t1 = time.time()
@@ -100,4 +100,4 @@ def test_parallel_max_wait():
     with ParallelRun() as parallel:
         parallel.add_task(time.sleep, args=(1,))
         with pytest.raises(TimeoutError):
-            parallel.wait(interval=.1, max_wait=.5)
+            parallel.wait(interval=0.1, max_wait=0.5)
